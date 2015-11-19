85b004b1b91d5e424f866d2e5e6e5d635cb6aba6
P3PPP <ticktackmob@gmail.com>
2015-11-19 17:56:24 +0900

    add :  XamlAnimations.cs

---
+++ XFXamlAnimations/XamlAnimations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace XFXamlAnimations
+{
+	[Preserve(AllMembers = true)]
+	public static class XamlAnimations
+	{
+		public static void Init()
+		{
+			var tmp = DateTime.Now;
+		}
+	}
+}
+
