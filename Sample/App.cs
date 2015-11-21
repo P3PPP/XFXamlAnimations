feed62b57fb2a6c0d1582254b0e0b00639c07b2d
P3PPP <ticktackmob@gmail.com>
2015-11-22 03:07:25 +0900

    add: sample projects

---
+++ Sample/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Sample
+{
+	public class App : Application
+	{
+		public App()
+		{
+			XFXamlAnimations.XamlAnimations.Init();
+			MainPage = new TabbedPage {
+				Children = {
+					new ViewExtentionsAnimationPage {
+						Title = "ViewExtentions animaton"
+					},
+					new PropertynAnimationPage {
+						Title = "Property animation"
+					},
+				},
+			};
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ Sample/ViewExtentionsAnimationPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Sample
+{
+	public partial class ViewExtentionsAnimationPage : ContentPage
+	{
+		public ViewExtentionsAnimationPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
